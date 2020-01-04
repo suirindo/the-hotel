9bb4eb04bbb5f3016ded7eb5b05942f94f66b4c1
TomohiIida <lagavulin.jb.dc@gmail.com>
2020-01-05 01:13:36 +0900

    Execute linting on main.py

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
 from flask import Flask, request, abort
 
-from linebot import (
-   LineBotApi, WebhookHandler
-)
-from linebot.exceptions import (
-   InvalidSignatureError
-)
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
 from linebot.models import (
-   MessageEvent, TextMessage, TextSendMessage,
+    MessageEvent,
+    TextMessage,
+    TextSendMessage,
 )
 
 import os
@@ -21,41 +19,47 @@ YOUR_CHANNEL_SECRET = os.environ["YOUR_CHANNEL_SECRET"]
 line_bot_api = LineBotApi(YOUR_CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(YOUR_CHANNEL_SECRET)
 
+
 @app.route("/")
 def hello_world():
-   return "hello world!"
+    return "hello world!"
 
-@app.route("/callback", methods=['POST'])
+
+@app.route("/callback", methods=["POST"])
 def callback():
-   # get X-Line-Signature header value
-   signature = request.headers['X-Line-Signature']
+    # get X-Line-Signature header value
+    signature = request.headers["X-Line-Signature"]
 
-   # get request body as text
-   body = request.get_data(as_text=True)
-   app.logger.info("Request body: " + body)
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
 
-   # handle webhook body
-   try:
-       handler.handle(body, signature)
-   except InvalidSignatureError:
-       print("Invalid signature. Please check your channel access token/channel secret.")
-       abort(400)
+    # handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        print(
+            "Invalid signature. Please check your channel access token/channel secret."
+        )
+        abort(400)
 
-   return 'OK'
+    return "OK"
+
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
 
-   if event.reply_token == "00000000000000000000000000000000":
-      return
-   push_text = event.message.text
-   results = hotel.extract_words(push_text)
-   if isinstance(results, tuple):
-       msg = hotel.hotel_search(*results)
-   else:
-       msg = results
-   line_bot_api.reply_message(event.reply_token,TextSendMessage(text=msg))
+    if event.reply_token == "00000000000000000000000000000000":
+        return
+    push_text = event.message.text
+    results = hotel.extract_words(push_text)
+    if isinstance(results, tuple):
+        msg = hotel.hotel_search(*results)
+    else:
+        msg = results
+    line_bot_api.reply_message(event.reply_token, TextSendMessage(text=msg))
+
 
 if __name__ == "__main__":
-   port = int(os.getenv("PORT"))
-   app.run(host="0.0.0.0", port=port)
+    port = int(os.getenv("PORT"))
+    app.run(host="0.0.0.0", port=port)
